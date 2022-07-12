52b0b54b520dbcb7bc62f92ee4b21b5d856cbfc0
Anton Lukashevich <luka14a@gmail.com>
2022-07-12 10:27:49 +0300

    .env

--- src/enviroments/environment.prod.ts
+++ src/enviroments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   backEndUrl: 'https://demo-lyrics-api.herokuapp.com'
+  //backEndUrl: process.env['BACKEND_ULR ']
 }
--- src/enviroments/environment.ts
+++ src/enviroments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
   backEndUrl: 'https://demo-lyrics-api.herokuapp.com'
+  //backEndUrl: process.env['BACKEND_ULR ']
 }
